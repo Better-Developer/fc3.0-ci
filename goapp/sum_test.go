5daa0d3eaeaaad3c4dd042b83c1defd50ea78f88
Anibal Antonio <djani454@gmail.com>
2022-09-04 01:18:50 +0100

    fix: fix func sum line 5 expected dectaration
    fix sum.go test unit test

--- goapp/sum_test.go
+++ goapp/sum_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-fnc testSum(t *testing.T) {
+func testSum(t *testing.T) {
 	result := sum(2,3)
 	if (result != 5) {
 		t.Errorf("Inavlid! \n result: %d -> expected: %d", result, 5)
